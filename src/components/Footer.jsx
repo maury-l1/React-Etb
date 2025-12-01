14313a23abdf5aa08f0b62573f6c75ff68970967
maury <mauricio.huamani.7e8@itb.cat>
2025-12-01 20:16:25 +0100

    ft styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,25 +4,34 @@ class Footer extends React.Component {
   render() {
     return (
       <footer className="bg-brown footer border-t border-white/10">
-        <div className='grid gap-6 md:grid-cols-4 w-3/5'>
-          <div className='w-3/5'>
+        <div className='max-w-6xl mx-auto px-4 sm:px-6 py-10 grid gap-6 md:grid-cols-4'>
+   
             <div className='md:col-span-2'>
               <div>
-                <span className='text-white'>Encuentra tu bolo</span>
+                <span className='text-white font-bold'>Encuentra tu bolo</span>
               </div>
               <p className='text-white'>Amplía tus oportunidades musicales: publica tus bolos y descubre locales que vibran contigo.</p>
             </div>
             <div>
               <div>
-                <span className='text-white'>Contacto</span>
+                <span className='text-white font-bold'>Contacto</span>
+                <ul>
+                  <li className='text-white'>Correo: encuentratubolo@gmail.com</li>
+                </ul>
               </div>
             </div>
             <div>
               <div>
-                <span className='text-white'>Social</span>
+                <span className='text-white font-bold'>Social</span>
+                  <ul>
+                  <li className='text-white'>Instagram </li>
+                  <li className='text-white'>Youtube </li>
+                  <li className='text-white'>X </li>
+                  <li className='text-white'>Spotify </li>
+                </ul>
               </div>
             </div>
-          </div>
+         
         </div>
       </footer>
 
--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -16,7 +16,7 @@ class Items extends React.Component {
             <h2 className='text-amber-50 text-4xl'>{title}</h2>
           </div>
         </div>
-        <div className="h-auto flex justify-center space-x-16 bg-yellow-400 w-3/5 mx-auto">
+        <div className="h-auto flex justify-center space-x-16 bg-red w-3/5 mx-auto">
 
 
           {list.map((item) => {
