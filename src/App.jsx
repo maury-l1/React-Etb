56866f3082945b69299f932dac89ce15bc0bdc20
maury <mauricio.huamani.7e8@itb.cat>
2025-12-18 16:02:17 +0100

    ft routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Home from './components/Home';
 import Navbar from './components/NavBar';
 import Perfil from './components/Perfil'
+import Contacto from './components/Contacto'
 
 
 class App extends React.Component {
@@ -14,6 +15,7 @@ class App extends React.Component {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Perfil" element={<Perfil />} />
+          <Route path="/Contacto" element={<Contacto />} />
         </Routes>
       </div>
     );
